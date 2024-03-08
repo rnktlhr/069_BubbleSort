9c16a812cade63efcf247464c727c7bb09a5651e
rnktlhr <rinakitluhur10@gmail.com>
2024-03-08 15:23:16 +0700

    menambah input

--- Latihan Bubbleshort/Latihan Bubbleshort.cpp
+++ Latihan Bubbleshort/Latihan Bubbleshort.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
+int arr[20]
+int n;
+
+void input() {
+    
+    while (true) {
+        cout << "Masukkan banyaknya elemen pada array: ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"
+        }
+    }
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukan Elemen Array" << endl;
+    cout <<
+}
 
 int main()
 {
