9c6813c487245a9791e27b278a26d38342ed4d21
rnktlhr <rinakitluhur10@gmail.com>
2024-03-08 15:56:46 +0700

    menambahkan display

--- Latihan Bubbleshort/Latihan Bubbleshort.cpp
+++ Latihan Bubbleshort/Latihan Bubbleshort.cpp
@@ -49,6 +49,27 @@ void bubbleSortArray() {
         cout << endl;
     } while (pass <= n - 1);
 }
+
+void display() {
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang Telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+
+    for (int j = 0; j < n; j++) {
+        cout << arr[j];
+        if (j < n - 1) {
+            cout << "--->";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "Jmlah pass = " << n - 1 << endl;
+    cout << endl;
+    cout << endl; 
+}
 int main()
 {
     std::cout << "Hello World!\n";
