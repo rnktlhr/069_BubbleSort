12b386569b541578172ce7e515c67dfc1e2e448a
rnktlhr <rinakitluhur10@gmail.com>
2024-03-08 15:50:59 +0700

    menambahkan bubble sort

--- Latihan Bubbleshort/Latihan Bubbleshort.cpp
+++ Latihan Bubbleshort/Latihan Bubbleshort.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-int arr[20]
+int arr[20];
 int n;
 
 void input() {
@@ -13,15 +13,42 @@ void input() {
         if (n <= 20)
             break;
         else {
-            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
         }
     }
     cout << endl;
     cout << "=====================" << endl;
     cout << "Masukan Elemen Array" << endl;
-    cout <<
+    cout << "=====================" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << (i + 1) << ":";
+        cin >> arr[i];
+
+    }
 }
 
+void bubbleSortArray() {
+    int pass = 1;
+
+    do {
+        for (int j = 0; j <= n - 1 - pass; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+
+       }
+        pass = pass + 1;
+
+        cout << "\nPass " << pass - 1 << ":";
+        for (int k = 0; k < n; k++) {
+            cout << arr[k] << "";
+        }
+        cout << endl;
+    } while (pass <= n - 1);
+}
 int main()
 {
     std::cout << "Hello World!\n";
